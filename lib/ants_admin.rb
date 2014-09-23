b8089d0a1031d1735d5f2a519421143ac142c8c0
Karl <karl@rubify.com>
2014-09-23 17:01:15 +0700

    add install

--- lib/ants_admin.rb
+++ lib/ants_admin.rb
@@ -1,4 +1,8 @@
+require 'rails'
 module AntsAdmin
+  def self.setup
+    yield self        
+  end
   class Engine < Rails::Engine
   end
 end
--- lib/generators/ants_admin/install_generator.rb
+++ lib/generators/ants_admin/install_generator.rb
@@ -1,20 +1,24 @@
 require 'securerandom'
 require 'rails/generators'
 require 'rails/generators/migration'
+require 'rails/generators/named_base'
 
 module AntsAdmin
-  class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path("../templates", __FILE__)
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      include Rails::Generators::ResourceHelpers
+      namespace "ants_admin"
+      source_root File.expand_path("../templates", __FILE__)
     
-    # Copy config file for my gem
-    def create_file_config(a)
-      puts a 
-      # template "config/express_translate.yml", "config/express_translate.yml"
-    end
+      # Copy config file for my gem
+      def create_file_config(a)
+        puts "a"
+        # template "config/express_translate.yml", "config/express_translate.yml"
+      end
     
-    def karl
-     
+      def karl
+        puts "b"
+      end
     end
   end
 end
-
