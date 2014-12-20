1e4e1cdde73eed655aebaa3188be78c0ca398719
Karl <thanhkhoait@gmail.com>
2014-12-20 09:17:09 +0700

    fix Mailer

--- app/mailers/ants_admin/mailer.rb
+++ app/mailers/ants_admin/mailer.rb
@@ -1,20 +1,20 @@
-if defined?(ActionMailer)
-  class AntsAdmin::Mailer < AntsAdmin.parent_mailer.constantize
-    include AntsAdmin::Mailers::Helpers
-
-    def confirmation_instructions(record, token, opts={})
-      @token = token
-      ants_admin_mail(record, :confirmation_instructions, opts)
-    end
-
-    def reset_password_instructions(record, token, opts={})
-      @token = token
-      ants_admin_mail(record, :reset_password_instructions, opts)
-    end
-
-    def unlock_instructions(record, token, opts={})
-      @token = token
-      ants_admin_mail(record, :unlock_instructions, opts)
-    end
-  end
-end
+# if defined?(ActionMailer)
+#   class AntsAdmin::Mailer < AntsAdmin.parent_mailer.constantize
+#     include AntsAdmin::Mailers::Helpers
+#
+#     def confirmation_instructions(record, token, opts={})
+#       @token = token
+#       ants_admin_mail(record, :confirmation_instructions, opts)
+#     end
+#
+#     def reset_password_instructions(record, token, opts={})
+#       @token = token
+#       ants_admin_mail(record, :reset_password_instructions, opts)
+#     end
+#
+#     def unlock_instructions(record, token, opts={})
+#       @token = token
+#       ants_admin_mail(record, :unlock_instructions, opts)
+#     end
+#   end
+# end
